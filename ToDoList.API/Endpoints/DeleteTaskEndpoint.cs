4578b9d8e4a08e604226f0a599ce3b167ed6502a
Mandraszewsky <mandrah@o2.pl>
2024-11-09 16:14:14 +0100

    Delete and Update endpoints.

---
+++ ToDoList.API/Endpoints/DeleteTaskEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+using MediatR;
+using ToDoList.Application.Tasks.Commands.DeleteTask;
+
+namespace ToDoList.API.Endpoints;
+
+public class DeleteTask : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteTaskCommand(Id));
+
+            return Results.Ok(result);
+        })
+        .WithName("DeleteTask")
+        .Produces<DeleteTaskResult>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Task")
+        .WithDescription("Delete Task");
+    }
+}
---
+++ ToDoList.API/Endpoints/UpdateTaskEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using MediatR;
+using ToDoList.Application.Tasks.Commands.UpdateTask;
+
+namespace ToDoList.API.Endpoints;
+
+public class UpdateTask : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders", async (UpdateTaskCommand command, ISender sender) =>
+        {
+            var result = await sender.Send(command);
+
+            return Results.Ok(result);
+        })
+        .WithName("UpdateTask")
+        .Produces<UpdateTaskResult>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Task")
+        .WithDescription("Update Task");
+    }
+}
