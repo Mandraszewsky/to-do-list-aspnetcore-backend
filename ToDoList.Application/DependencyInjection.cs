b82074df720aee177284f68edc54810258f60b11
Mandraszewsky <mandrah@o2.pl>
2024-11-10 17:19:42 +0100

    Behaviors for logging and validation (mediatR).

--- ToDoList.Application/DependencyInjection.cs
+++ ToDoList.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using ToDoList.Application.Behaviors;
 
 namespace ToDoList.Application;
 
@@ -10,8 +12,11 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 
