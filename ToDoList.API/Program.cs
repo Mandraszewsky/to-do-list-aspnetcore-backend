21a9f471a68cd29e212310cd30f8b7897655705f
Mandraszewsky <mandrah@o2.pl>
2024-11-10 09:15:11 +0100

    CORS added.

--- ToDoList.API/Program.cs
+++ ToDoList.API/Program.cs
@@ -12,11 +12,23 @@ builder.Services
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices();
 
+var allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins")!.Split(",");
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(allowedOrigins).AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipelines:
 
 app.UseApiServices();
+
+app.UseCors();
 
 if (app.Environment.IsDevelopment())
 {
